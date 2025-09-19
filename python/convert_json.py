2dafc71bf33efc6759212c33b7c8f85e5807aa5d
Shamiul <anik.savar.bd@gmail.com>
2025-09-19 23:26:55 +0900

    Logo and code structure update

--- python/convert_json.py
+++ python/convert_json.py
@@ -3,14 +3,14 @@ import json
 def convert_json_to_js():
     try:
         # Read the JSON file
-        with open('../vocabulary.json', 'r', encoding='utf-8') as json_file:
+        with open('../data/vocabulary.json', 'r', encoding='utf-8') as json_file:
             data = json.load(json_file)
         
         # Create the JavaScript file content
         js_content = 'const vocabularyData = ' + json.dumps(data, ensure_ascii=False, indent=2) + ';'
         
         # Write to the JavaScript file
-        with open('data.js', 'w', encoding='utf-8') as js_file:
+        with open('../js/data.js', 'w', encoding='utf-8') as js_file:
             js_file.write(js_content)
             
         print("Successfully converted vocabulary.json to data.js!")
